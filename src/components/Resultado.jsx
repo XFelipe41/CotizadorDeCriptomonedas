51d27a2f1c014c788b646d89bfab117511ee8547
XFelipe41 <jfcaicedot@ut.edu.co>
2023-09-08 10:22:00 -0500

    Proyecto CDCM

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,61 @@
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Resultado = ({ resultado }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+  return (
+    <Contenedor>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen cripto"
+      />
+      <div>
+        <Precio>
+          El Precio Es De: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio Más Alto del Día: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio Más Bajo del Día : <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variación Últimas 24 Horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Última Actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
+  );
+};
+
+export default Resultado;
